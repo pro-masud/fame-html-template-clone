535bc2649eaf0ce86427a022103f7f0d58ee892c
pro-mannan <promasudbd@gmail.com>
2023-07-16 16:36:58 +0600

    fame portfolio section complete

--- js/script.js
+++ js/script.js
@@ -7,3 +7,26 @@ $(window).load(function () {
     "use strict";
 	$('#loader').fadeOut();
 });
+
+
+// jQuery for page scrolling feature -reuires jQuery Easing plugin
+
+$(function () {
+    
+    "use strict";
+    
+    $('a.page-scroll').bind('click', function (event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top - 68
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
+
+
+$('body').scrollspy({
+    target: '.navbar-fixed-top',
+	offset: 70
+	
+});

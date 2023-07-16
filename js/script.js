ca36ad72a5a3db52fad90d7ecf5965cd08b34f1e
pro-mannan <promasudbd@gmail.com>
2023-07-16 19:49:36 +0600

    fame progress bar complete now

--- js/script.js
+++ js/script.js
@@ -30,3 +30,22 @@ $('body').scrollspy({
 	offset: 70
 	
 });
+
+
+
+// progress bar
+
+$(document).ready(function($) {
+	"use strict";
+    
+        $('.skill-shortcode').appear(function() {
+            $('.progress').each(function(){ 
+                $('.progress-bar').css('width',  function(){ 
+                    return ($(this).attr('data-percentage')+'%')
+                });
+            });
+        },{accY: -100});
+
+        
+    });
+	

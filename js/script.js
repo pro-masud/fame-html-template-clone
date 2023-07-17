200d0ef57882748c2738201dc15b879ac56e073e
pro-mannan <promasudbd@gmail.com>
2023-07-17 12:11:40 +0600

    Team Member Section Complete now

--- js/script.js
+++ js/script.js
@@ -57,4 +57,24 @@ $('.timer').countTo();
 $('.counter-item').appear(function() {
     $('.timer').countTo();
 },{accY: -100});
-	
+
+
+
+
+// Our Team Carousel
+
+$(document).ready(function(){
+    // get our-team class
+    $(".our-team").owlCarousel({
+        pagination: true,
+        slideSpeed : 2500,
+		stopOnHover: true,
+    	autoPlay: 3000,
+        items: 4,
+    	//singleItem:true,
+        itemsMobile : [550,1],
+        itemsDesktopSmall : [991,2],
+		transitionStyle : "fade",
+		navigationText: ['<i class="fa fa-chevron-left"></i>','<i class="fa fa-chevron-right"></i>']
+  });
+});

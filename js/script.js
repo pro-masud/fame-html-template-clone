99c5f87f7d0ac5ed3867ac1c1c2a84c15c587145
pro-mannan <promasudbd@gmail.com>
2023-07-17 12:56:32 +0600

    pricing table section complete now

--- js/script.js
+++ js/script.js
@@ -78,3 +78,22 @@ $(document).ready(function(){
 		navigationText: ['<i class="fa fa-chevron-left"></i>','<i class="fa fa-chevron-right"></i>']
   });
 });
+
+
+// Our Pricing Table
+
+$(document).ready(function(){
+    // get our-team class
+    $(".pricing").owlCarousel({
+        pagination: true,
+        slideSpeed : 2500,
+		stopOnHover: true,
+    	autoPlay: 3000,
+        items: 4,
+    	//singleItem:true,
+        itemsMobile : [550,1],
+        itemsDesktopSmall : [991,2],
+		transitionStyle : "fade",
+		navigationText: ['<i class="fa fa-chevron-left"></i>','<i class="fa fa-chevron-right"></i>']
+  });
+});
